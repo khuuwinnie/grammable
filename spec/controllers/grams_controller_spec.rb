45dcdefaa73f10368678b9c76748a7cd486f777e
Winnie Khuu <khuu.winnie@gmail.com>
2019-09-03 12:02:50 +0000

    adding Grams to the database testing - 1 error occurring outside of examples

--- spec/controllers/grams_controller_spec.rb
+++ spec/controllers/grams_controller_spec.rb
@@ -1,9 +1,12 @@
 require 'rails_helper'
 
 RSpec.describe GramsController, type: :controller do
-  describe "grams#index action" do
-    it "should successfully show the page" do
-      get :index
+  
+end
+
+  describe "grams#new action" do
+    it "should successfully show the new form" do
+      get :new
       expect(response).to have_http_status(:success)
     end
   end
